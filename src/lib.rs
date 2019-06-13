ca073a6a6bd980d18b3f92c2eeb2b5b1474bd40f
Corin Lawson <corin@responsight.com>
2019-06-13 14:07:59 +1000

    Introduce ExitStatus struct
    Prior to this change, the only means to customise the error message was
    via the `Display` trait.
    
    This change introduces the `ExitStatus` struct and defaults the type
    parameter of `Exit` to be this struct.  Custom error types can continue
    to implement `From<T> for i32` but for full control they can instead (or
    in addition to) implement `From<T> for exit::ExitStatus`.
    
    Note that implementing `From<T> for exit::ExitStatus` requires the
    implementor's crate to opt-in to the unstable specialization feature.
    
    Signed-off-by: Corin Lawson <corin@responsight.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,48 @@
 #![feature(termination_trait_lib)]
 #![feature(try_trait)]
+#![feature(process_exitcode_placeholder)]
+#![feature(specialization)]
 
 use std::fmt::Debug;
 use std::ops::Try;
-use std::process::Termination;
+use std::process::{ExitCode, Termination};
 
-pub enum Exit<T> {
+pub struct ExitStatus {
+    pub status_code: i32,
+    pub error: String,
+}
+
+impl<T: Debug> From<T> for ExitStatus {
+    default fn from(t: T) -> ExitStatus {
+        ExitStatus {
+            status_code: ExitCode::FAILURE.report(),
+            error: format!("{:?}", t),
+        }
+    }
+}
+
+impl<T: Into<i32> + Debug> From<T> for ExitStatus {
+    default fn from(t: T) -> ExitStatus {
+        ExitStatus {
+            error: format!("{:?}", t),
+            status_code: t.into(),
+        }
+    }
+}
+
+pub enum Exit<T = ExitStatus> {
     Ok,
     Err(T),
 }
 
-impl<T: Into<i32> + Debug> Termination for Exit<T> {
-    fn report(self) -> i32 {
+impl<T: Into<ExitStatus>> Termination for Exit<T> {
+    default fn report(self) -> i32 {
         match self {
-            Exit::Ok => 0,
+            Exit::Ok => ExitCode::SUCCESS.report(),
             Exit::Err(err) => {
-                eprintln!("Error: {:?}", err);
-                err.into()
+                let err = err.into();
+                eprintln!("Error: {}", err.error);
+                err.status_code
             }
         }
     }
@@ -26,18 +52,18 @@ impl<T> Try for Exit<T> {
     type Ok = ();
     type Error = T;
 
-    fn into_result(self) -> Result<<Self as Try>::Ok, Self::Error> {
+    default fn into_result(self) -> Result<<Self as Try>::Ok, Self::Error> {
         match self {
             Exit::Ok => Ok(()),
             Exit::Err(err) => Err(err),
         }
     }
 
-    fn from_error(err: Self::Error) -> Self {
+    default fn from_error(err: Self::Error) -> Self {
         Exit::Err(err)
     }
 
-    fn from_ok(_: <Self as Try>::Ok) -> Self {
+    default fn from_ok(_: <Self as Try>::Ok) -> Self {
         Exit::Ok
     }
 }
